ea60358dcc0275d9a7e4befe030bb3681301e1b9
vivek <vivek.patial@hoolime.co>
2021-03-16 12:40:04 +0530

    create read backend func added

--- to-do-full-stack/src/components/Notes/Main/Main.js
+++ to-do-full-stack/src/components/Notes/Main/Main.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import SideNav from '../SideNav/SideNav'
 import './Main.css'
 import {useDispatch, useSelector} from 'react-redux'
@@ -19,9 +19,19 @@ function Main() {
     }
     const addnotes = () => {
         console.log(note)
-        axios.post('http://localhost:7777/task',note)
-
+        axios.post('http://localhost:7777/task',note).then(res=>{
+            fetchFromDb()
+            console.log(res)})
     }
+    const fetchFromDb = async() =>{
+        const res = await axios.get('http://localhost:7777/task')
+        console.log(res)
+        console.log(res.data)
+    }
+    useEffect(()=>{
+        // will use redux saga after this
+        fetchFromDb()
+    },[])
     return (
         <div>
             <h1>NeverNote</h1>
